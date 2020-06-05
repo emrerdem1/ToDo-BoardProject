89316f41b055894faae03740273deb8fcf5130a6
Burak <burakselvi858@hotmail.com>
2020-06-05 22:13:04 +0300

    fields added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import db from "./firebaseConfig";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const priorities = [
+    { value: 0, name: "low" },
+    { value: 1, name: "medium" },
+    { value: 2, name: "high" },
+  ];
+  const statusFlags = [
+    { value: 0, name: "todo" },
+    { value: 1, name: "planned" },
+    { value: 2, name: "inProgress" },
+    { value: 3, name: "done" },
+    { value: 4, name: "testing" },
+  ];
   const [boards, setBoards] = useState([]);
-
-  console.log(boards);
+  const { register, handleSubmit } = useForm();
 
   useEffect(() => {
     getBoards();
   }, []);
+
+  const onSubmit = (data) => {
+    const sendingData = {
+      ...data,
+      priority: parseInt(data.priority),
+      statusFlag: parseInt(data.statusFlag),
+      dueDate: Date.parse(data.dueDate),
+    };
+    console.log(sendingData);
+    addBoard(sendingData);
+  };
 
   const getBoards = async () => {
     const response = await db.collection("boards").get();
@@ -20,7 +43,86 @@ function App() {
     setBoards([...data]);
   };
 
-  return <div className="App"></div>;
+  const addBoard = (board) => {
+    db.collection("boards").add(board);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="title">Title: </label>
+        <input name="title" ref={register({ required: true })} />
+        <br />
+        <label htmlFor="subtitle">Subtitle: </label>
+        <input name="subtitle" ref={register()} />
+        <br />
+        <label htmlFor="assignee">Assignee: </label>
+        <input name="assignee" ref={register()} />
+        <br />
+        <label htmlFor="explanation">Explanation: </label>
+        <input name="explanation" ref={register()} />
+        <br />
+        <label htmlFor="priority">Priorities: </label>
+        <select name="priority" id="priority" ref={register()}>
+          {priorities.map((priority) => {
+            return (
+              <option key={priority.value} value={priority.value}>
+                {priority.name}
+              </option>
+            );
+          })}
+        </select>
+        <br />
+        <label htmlFor="statusFlag">Choose a status flag:</label>
+        <select name="statusFlag" id="statusFlag" ref={register()}>
+          {statusFlags.map((flag) => {
+            return (
+              <option key={flag.value} value={flag.value}>
+                {flag.name}
+              </option>
+            );
+          })}
+        </select>
+        <br />
+        <label htmlFor="dueDate">Bitiş Tarihi: </label>
+        <input
+          type="date"
+          id="dueDate"
+          name="dueDate"
+          defaultValue={1593475200000}
+          ref={register()}
+        />
+        <br />
+        <button type="submit">Kaydet</button>
+      </form>
+      {boards.map(
+        ({
+          id,
+          title,
+          subtitle,
+          assignee,
+          explanation,
+          dueDate,
+          priority,
+          statusFlag,
+        }) => {
+          return (
+            <div key={id}>
+              <p>Title: {title} </p>
+              <p>Subtitle: {subtitle}</p>
+              <p>Assignee: {assignee}</p>
+              <p>Explanation: {explanation}</p>
+              <p>
+                Due Date: {new Intl.DateTimeFormat("en-US").format(dueDate)}
+              </p>
+              <p>Priority: {priority}</p>
+              <p>StatusFlag: {statusFlag}</p>
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
 }
 
 export default App;
